ee6616f644ffb65bed59ad911f80c715d164c70b
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2024-02-01 23:38:20 +0530

    Time: 20 ms (99.06%) | Memory: 61.6 MB (5.21%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -1,54 +1,12 @@
-// class Solution {
-//     public int[][] divideArray(int[] nums, int k) {
-//         Arrays.sort(nums);
-//         int n=nums.length/3;
-//         int x=0;
-//         int array[][]={};
-//         int ans[][]=new int[n][3];
-//         int z=1;
-//         while(z<nums.length){
-//             // if(i%3!=0 && nums[i]-nums[i-1]>k){
-//             //     return array;
-//             // }
-//             // if(z%3==0){
-//             //     z++;
-//             // }else{
-//                 if(nums[z]-nums[z-1]>k){
-//                     return array;
-//                 }else{
-//                     z++;
-//                 }
-//             }
-//         // }
-        
-//         for(int i=0;i<n;i++){
-//             for(int j=0;j<3;j++){
-//                 ans[i][j]=nums[x];
-//                 x++;
-//             }
-//         }
-//         return ans;
-        
-//     }
-// }
-
-public class Solution {
+class Solution {
     public int[][] divideArray(int[] nums, int k) {
         Arrays.sort(nums);
-        int[][] result = new int[0][0];
-        
-        for (int i = 0; i + 2 < nums.length; ++i) {
-            if (i % 3 == 0) {
-                if (nums[i + 2] - nums[i] <= k) {
-                    int[] triplet = {nums[i], nums[i + 1], nums[i + 2]};
-                    result = Arrays.copyOf(result, result.length + 1);
-                    result[result.length - 1] = triplet;
-                } else {
-                    return new int[0][0];
-                }
-            }
+        List<int[]> res = new ArrayList<>();
+        for (int i = 2; i < nums.length; i += 3) {
+            if (nums[i] - nums[i - 2] > k) return new int[][]{};
+            res.add(new int[]{nums[i - 2], nums[i - 1], nums[i]});
         }
+        return res.toArray(new int[res.size()][]);
         
-        return result;
     }
 }
