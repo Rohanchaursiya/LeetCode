70105828878d1c6bf485ef8c9c4f6458512775ec
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2024-02-18 11:47:29 +0530

    Time: 16 ms (95.34%) | Memory: 45.2 MB (77.16%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.java
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -1,23 +1,35 @@
 class Solution {
-    public boolean isArithmetic(ArrayList<Integer> li){
-        int d=li.get(1)-li.get(0);
-        for(int i=1;i<li.size();i++){
-            if(li.get(i)-li.get(i-1)!=d)return false;
+
+    static boolean check(int arr[]){
+        int n = arr.length;
+
+        if(n==1) return true;
+
+        Arrays.sort(arr);
+
+        int diff = arr[1]-arr[0];
+
+        for(int i=1; i<n; i++){
+            if(arr[i]-arr[i-1]!=diff) return false;
         }
+
         return true;
     }
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        ArrayList<Boolean> ans=new ArrayList<Boolean>();
-        for(int j=0;j<l.length;j++){
-            ArrayList<Integer> li=new ArrayList<>();
-            for(int i=l[j];i<=r[j];i++){
-                li.add(nums[i]);
+
+        ArrayList<Boolean> ans = new ArrayList<>();
+        
+        for(int i=0; i<l.length; i++){
+            int n = r[i]-l[i]+1;
+            int arr[] = new int[n];
+            int idx = 0;
+
+            for(int j=l[i]; j<=r[i]; j++){
+                arr[idx++] = nums[j];
             }
-            Collections.sort(li);
-            boolean val=isArithmetic(li);
-            ans.add(val);
+            if(check(arr)) ans.add(true);
+            else ans.add(false);
         }
         return ans;
-        
     }
 }
