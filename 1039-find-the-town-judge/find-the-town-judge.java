43a4a32fb83f7a69b3c8c493d456e1ef916e4c25
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2024-02-22 11:49:41 +0530

    Time: 3 ms (71.04%) | Memory: 54.1 MB (21.53%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,24 +1,17 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if(n==1)return 1;
-        ArrayList<Integer> li=new ArrayList<>();
-        ArrayList<Integer> l=new ArrayList<>();
+        int trusting[]=new int[n+1];
+        int trusted[]=new int[n+1];
+
         for(int i=0;i<trust.length;i++){
-            li.add(trust[i][0]);
-            l.add(trust[i][1]);
+            trusting[trust[i][0]]++;
+            trusted[trust[i][1]]++;
         }
         for(int i=1;i<=n;i++){
-            if(!li.contains(i)){
-                if(l.contains(i)){
-                    int c=0;
-                    for(int j=0;j<l.size();j++){
-                        if(l.get(j)==i)c++;
-                    }
-                    if(c==n-1)return i;
-                }
-            }
+            if(trusting[i]==0 && trusted[i]==n-1)return i;
         }
         return -1;
+        
         
     }
 }
