4f3f6b9a6fe158f5514ea27244d8fad5ac756733
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2024-04-16 14:29:19 +0530

    Time: 1 ms (100.00%) | Memory: 79.5 MB (42.18%) - LeetSync

---
+++ 3373-maximum-prime-difference/maximum-prime-difference.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPrime(int n){
+        if(n==0 || n==1)return false;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0)return false;
+        }
+        return true;
+    }
+    public int maximumPrimeDifference(int[] nums) {
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j){
+            if(!isPrime(nums[i])){
+                i++;
+            }else{
+                break;
+            }
+        }
+        while(i<=j){
+            if(!isPrime(nums[j])){
+                j--;
+            }else{
+                break;
+            }
+        }
+        return j-i;
+    }
+}
