34afe335614c4b65a9f0c07cc0107f71d5a3b663
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2025-01-15 22:58:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (74.53%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimizeXor(int num1, int num2) {
+        int bits = Integer.bitCount(num2);
+        int result = 0;
+        for (int i = 31; i >= 0 && bits > 0; i--) {
+            if ((num1 & (1 << i)) != 0) {
+                result |= (1 << i);
+                bits--;
+            }
+        }
+        for (int i = 0; i < 32 && bits > 0; i++) {
+            if ((result & (1 << i)) == 0) {
+                result |= (1 << i);
+                bits--;
+            }
+        }
+        return result;
+    }
+}
