3bca8e4d810cb9027e0e319b2987bd4b10553521
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2025-09-09 18:14:45 +0530

    Time: 3 ms (85.45%) | Memory: 40.8 MB (60.00%) - LeetSync

---
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        final int mod=(int)1e9+7;
+        if(n==1){
+            return 1;
+        }
+        long dp[]=new long[n+1];
+        dp[1]=1;
+        long window=0;
+        for(int i=2;i<=n;i++){
+            int enter=i-delay;
+            int exit=i-forget;
+            if(enter>=1){
+                window=(window+dp[enter])%mod;
+            }
+            if(exit>=1){
+                window=(window-dp[exit]+mod)%mod;
+            }
+            dp[i]=window;
+        }
+        long ans=0;
+        int start=Math.max(1,n-forget+1);
+        for(int i=start;i<=n;i++){
+            ans=(ans+dp[i])%mod;
+        }
+        return (int)ans;
+    }
+}
