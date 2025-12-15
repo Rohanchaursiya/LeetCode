874579f573d741ced91104588cafaf91d72843a5
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2025-12-15 22:24:14 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (30.47%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,7 +1,18 @@
 class Solution {
+    public static int binarySearch(int low, int high, int arr[], int target){
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target){
+                return mid;
+            }else if(arr[mid]<target){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
     public int search(int[] nums, int target) {
-        int index=Arrays.binarySearch(nums,target);
-        return index>=0 ? index : -1;
-        
+        return binarySearch(0, nums.length-1, nums, target);  
     }
 }
