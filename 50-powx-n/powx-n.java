738ad476194145937e631e2b653e871d1b259bae
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2025-12-25 17:29:49 +0530

    Time: 1 ms (8.45%) | Memory: 48.3 MB (16.05%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
-        
+        double result=1.0;
+        long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+
+        while(N>0){
+            if((N&1)==1){
+                result*=x;
+            }
+            x*=x;
+            N>>=1;
+        }
+        return result;
     }
 }
